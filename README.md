fb514dd6e68a874476e3dc0d3650cec7d8a73b14
Nicola Brisotto <nicolabrisotto@gmail.com>
2022-07-04 11:25:16 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Additional content for the course [Learn Python - Full Course for Beginners Tutorial](https://www.youtube.com/watch?v=rfscVS0vtbw)
---
+++ try-except/version-1.py
@@ -0,0 +1,6 @@
+"""
+Scenario 1: Try enter 4 -> It will work!
+Scenario 1: Try enter a random text: asdref -> It will not work! Out program is goint to throw an error!
+"""
+number = int(input("Enter a number: "))
+print(number)
